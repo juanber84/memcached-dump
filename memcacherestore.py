e7bec8623f66e289351e0507dc68ca7c99c3943a
Juanber <juanber84@gmail.com>
2014-09-15 22:17:45 +0200

    Add memcacherestore file

---
+++ memcacherestore.py
@@ -0,0 +1,31 @@
+"""
+apt-get install python-memcache
+apt-get install libmemcached-tools
+"""
+#!/usr/bin/python
+import sys
+import memcache
+import json
+
+out = "memcachedump"
+host = "127.0.0.1"
+port = "11211"
+
+if "-f" in sys.argv:
+        out = sys.argv[sys.argv.index("-f")+1]
+if "-h" in sys.argv:
+        host = sys.argv[sys.argv.index("-h")+1]
+if "-p" in sys.argv:
+        port = sys.argv[sys.argv.index("-p")+1]
+print "conecting..."
+s = memcache.Client([host+":"+port])
+print "reading dumpfile..."
+c = open(out, 'r').read()
+try:
+    decoded = json.loads(c)
+    for key in decoded:
+        s.set(str(key),str(decoded[key]))
+        print "inserting  "+key+"..."
+except (ValueError, KeyError, TypeError):
+    print "JSON format error"
+print "restore finished"
