7b30967b63d300c4582e799f3f55325b88205f0c
Licaon_Kter <licaon-kter@users.noreply.github.com>
2021-09-01 09:38:34 +0000

    Update to Python 3

--- memcacheddump.py
+++ memcacheddump.py
@@ -1,37 +1,38 @@
 """
-apt-get install python-memcache
-apt-get install libmemcache-tools
+apt-get install python3-memcache
+apt-get install libmemcached-tools
 """
 #!/usr/bin/python
 import sys
 import memcache
 import subprocess
 import json
+import re
 
 out = "memdump"
 host = "127.0.0.1"
 port = "11211"
 if "-f" in sys.argv:
-	out = sys.argv[sys.argv.index("-f")+1]
+        out = sys.argv[sys.argv.index("-f")+1]
 if "-h" in sys.argv:
-	host = sys.argv[sys.argv.index("-h")+1]
+        host = sys.argv[sys.argv.index("-h")+1]
 if "-p" in sys.argv:
-	port = sys.argv[sys.argv.index("-p")+1]
-print "conecting..."
+        port = sys.argv[sys.argv.index("-p")+1]
+print ("conecting...")
 hostport = host+":"+port
 s = memcache.Client([hostport])
-print "checking keys..."
-output = subprocess.check_output("memcdump --servers="+hostport, shell=True)
-data= output.split('\n')
+print ("checking keys...")
+output = subprocess.check_output("memcdump --servers="+hostport+"|| true", shell=True, text=True)
+data = re.split('\n+', output)
 lista = {}
-for key  in data:
+for key in data:
     if key != '':
         lista[key]=s.get(key)
-        print "dumping "+key+"..."
+        print ("dumping "+key+"...")
 jsonfinal = json.dumps(lista, ensure_ascii=False)
-print "writing output..."
+print ("writing output...")
 fileWrite = open(out, "w")
 fileWrite.write(jsonfinal)
 fileWrite.close()
-print "memcache file created"
-print "finish dump"
+print ("memcache file created")
+print ("finish dump")
